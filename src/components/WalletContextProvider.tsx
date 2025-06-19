dbd6e0c6ff96669deb44656ddee99bdfd234c4c9
t9fiction <sohail.sohailishaq@gmail.com>
2025-06-19 18:10:57 +0500

    solana basic config

---
+++ src/components/WalletContextProvider.tsx
@@ -0,0 +1,61 @@
+"use client";
+import React, { ReactNode, useMemo } from "react";
+import {
+  WalletProvider,
+  ConnectionProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import dynamic from "next/dynamic";
+
+const DynamicWalletMultiButton = dynamic(
+  () =>
+    import("@solana/wallet-adapter-react-ui").then(
+      (mod) => mod.WalletMultiButton
+    ),
+  { ssr: false }
+);
+
+interface WalletContextProviderProps {
+  children: ReactNode;
+}
+
+const WalletContextProvider: React.FC<WalletContextProviderProps> = ({
+  children,
+}) => {
+  const network =
+    process.env.NEXT_PUBLIC_SOLANA_NETWORK === "mainnet"
+      ? WalletAdapterNetwork.Mainnet
+      : WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(
+    () => process.env.NEXT_PUBLIC_SOLANA_RPC || clusterApiUrl(network),
+    [network]
+  );
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new TorusWalletAdapter(),
+    ],
+    []
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect={false}>
+        <WalletModalProvider>
+          <DynamicWalletMultiButton />
+          {children}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletContextProvider;
